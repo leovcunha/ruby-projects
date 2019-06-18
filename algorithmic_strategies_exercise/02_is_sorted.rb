33ab5cdcde3e0f41c156c5e82326235e1164a8f1
leovcunha <lvcunha@gmail.com>
2019-06-17 23:48:00 -0300

    scope & refs

---
+++ algorithmic_strategies_exercise/02_is_sorted.rb
@@ -0,0 +1,11 @@
+# Write a method, is_sorted(arr), that accepts an array of numbers as an arg.
+# The method should return true if the elements are in increasing order, false otherwise.
+# Do not use the built-in Array#sort in your solution :)
+def is_sorted(arr)
+    sort = true
+    arr.each_with_index {|el,i| sort = sort && el <= arr[i+1] if arr[i+1]}
+    sort
+end
+p is_sorted([1, 4, 10, 13, 15])       # => true
+p is_sorted([1, 4, 10, 10, 13, 15])   # => true
+p is_sorted([1, 2, 5, 3, 4 ])         # => false

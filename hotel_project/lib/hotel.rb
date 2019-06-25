82a56271225f9ee0ecb22bf2394e73a7ac171c10
leovcunha <lvcunha@gmail.com>
2019-06-25 18:59:58 +0000

    hotel

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,45 @@
 require_relative "room"
 
 class Hotel
-  
+  def initialize(name, hash)
+    @name = name
+    @rooms = {}
+
+    hash.each do |room, capacity|
+        @rooms[room] = Room.new(capacity)
+    end
+  end
+
+  def name
+    @name.split.map(&:capitalize).join(" ")
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def room_exists?(room)
+    return true if @rooms.include?(room)
+    false
+  end
+
+  def check_in(person, room)
+    if room_exists?(room)
+      if @rooms[room].add_occupant(person)
+        puts "check in successful"
+      else
+        puts "sorry, room is full"
+      end
+    else
+      puts "sorry, room does not exist"
+    end
+  end
+
+  def has_vacancy?
+    @rooms.values.any? { |room| !room.full? }
+  end
+
+  def list_rooms
+    @rooms.each { |name, room| puts "#{name} : #{room.available_space}"}
+  end
 end
--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,28 @@
 class Room
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
 
+    def capacity
+        @capacity
+    end
+    def occupants
+        @occupants
+    end
+
+    def full?
+        return false if @occupants.length < @capacity
+        true
+    end
+
+    def available_space
+        @capacity - @occupants.length
+    end
+
+    def add_occupant(name)
+        return false if full?
+        @occupants << name
+        true
+    end
 end
